0429e888ad15c3333e3bd0e982180378b2c68a8f
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2022-01-29 15:44:40 -0800

    prop drilling up and down

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,18 @@ function App(){
 
    const [feedback, setFeedback] = useState(FeedbackData)
 
-   
+   const deleteFeedback=(id) =>{console.log('App', id)
+    if(window.confirm('Are you sure you want to delete?')){
+        setFeedback(feedback.filter((item)=>item.id !== id))
+
+    }
+
+}
 
     return (
     <>
     <Header ></Header>
-   <FeedbackList list={feedback}></FeedbackList>
+   <FeedbackList list={feedback} handleDelete={(id)=> deleteFeedback(id)}></FeedbackList>
    
     </>
     )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
-function FeedbackList({list}) {
+function FeedbackList({list, handleDelete}) {
 
    
 
   return <div className='feedback-list'>
     
 {list && (
-list.map((item,idx) =>(
-    <FeedbackItem key={idx} item ={item}></FeedbackItem>
+list.map((item) =>(
+    <FeedbackItem key={item.id} item ={item}
+    handleDelete={handleDelete}
+    ></FeedbackItem>
 )))
 }
   </div>;
