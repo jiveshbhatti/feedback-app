1fbcf4254cd8020958c92019ec95e4c6cda3211e
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2022-01-30 10:18:04 -0800

    Feedback form min 10 char check

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,33 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Card from './shared/Card';
-import Button from './shared/Button';
-import { useState } from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+import { useState } from "react";
 const FeedbackForm = (props) => {
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  const handleTextChange = (val) => {
+  
+    setText(val);
+    setMessage("Text must be at least 10 characters");
 
-    const [text, setText] = useState('')
-
-    const handleTextChange = (val) => {
-        console.log(val)
-
-        setText(val)
+    if (text.trim().length > 10) {
+      setBtnDisabled(false);
+      setMessage(null);
     }
-  return <Card>
-
+  };
+  return (
+    <Card>
       <form>
-          <h2>How would you rate your service with us?</h2>
+        <h2>How would you rate your service with us?</h2>
 
-          <div className="input-group">
-              <input onChange={e => handleTextChange(e.target.value)}
-              value={
-                  text
-              } type="text" placeholder='Write a review' />
-              <Button type="submit" version='primary' >
-                  Send
-              </Button>
+        <div className="input-group">
+          <input
+            onChange={(e) => handleTextChange(e.target.value)}
+            value={text}
+            type="text"
+            placeholder="Write a review"
+          />
+
+          <Button type="submit" version="primary" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+
+        {message && (
+          <div
+            className="
+          message"
+          >
+            {message}
           </div>
+        )}
       </form>
-  </Card>
+    </Card>
+  );
 };
 
 FeedbackForm.propTypes = {};
